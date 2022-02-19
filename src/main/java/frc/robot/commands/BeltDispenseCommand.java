36e9d710367aa4d86dc1ab0159891c197943c1dc
Nathan George <69521182+Nathan-George@users.noreply.github.com>
2022-02-18 20:54:34 -0700

    added dispence command

--- src/main/java/frc/robot/commands/BeltDispenseCommand.java
+++ src/main/java/frc/robot/commands/BeltDispenseCommand.java
@@ -25,7 +25,7 @@ public class BeltDispenseCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    ballBelt.dispense();
   }
 
   // Called once the command ends or is interrupted.
